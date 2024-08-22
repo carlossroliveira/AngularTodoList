5f4f6b25acf738ddffd429cd4c2e0459996d95d2
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2024-08-22 17:08:18 -0300

    Feat: importando modal

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskModalComponent } from '../task-modal/task-modal.component'
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [TaskModalComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
