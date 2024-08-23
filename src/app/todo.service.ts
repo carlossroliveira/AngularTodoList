9321b271c15a9c75b119f4efa34903e214717574
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2024-08-22 23:26:43 -0300

    Feat: Implementação do TodoService

---
+++ src/app/todo.service.ts
@@ -0,0 +1,35 @@
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Task {
+  id?: number;
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+  deleteTask(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
+  }
+}
