45645153371c1f3668f229597f5a68702e926470
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2024-08-22 23:25:45 -0300

    Feat: Implementação do list component

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { TaskModalComponent } from '../task-modal/task-modal.component'
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Task, TodoService } from '../../todo.service';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [TaskModalComponent],
+  imports: [CommonModule],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrl: './task-list.component.scss',
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.fetchTasks();
+  }
+
+  fetchTasks(): void {
+    this.todoService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(id: number | undefined): void {
+    if (id !== undefined) {
+      this.todoService.deleteTask(id).subscribe(() => {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      });
+    }
+  }
+
+  toggleCompletion(task: Task): void {
+    task.completed = !task.completed;
+    this.todoService.updateTask(task).subscribe();
+  }
 }
