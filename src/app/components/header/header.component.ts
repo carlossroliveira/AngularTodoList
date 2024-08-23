4010aa9fa6b6e8b3b9a91a71ad556c5ccebb1415
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2024-08-22 23:25:57 -0300

    Feat: Implementação do header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Task, TodoService } from '../../todo.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  newTask: Task = {
+    title: '',
+    description: '',
+    completed: false,
+  };
 
+  constructor(private todoService: TodoService) {}
+
+  addTask(): void {
+    if (this.newTask.title && this.newTask.description) {
+      this.todoService.addTask(this.newTask).subscribe(() => {});
+      this.newTask = { title: '', description: '', completed: false };
+    }
+  }
 }
